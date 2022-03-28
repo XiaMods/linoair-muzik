9f810d30dda1321c61aca768bf582a2e73fe501c
XiaMods <95963443+XiaMods@users.noreply.github.com>
2022-03-29 00:06:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        TOKEN: 'OTQ5MjgwOTc3MTkxMDcxNzc0.YiIEzQ.ml8wLxl5hrj3pHpxYqBOVdil5ns', //your discord bot token
+        TOKEN: 'OTQ5MjgwOTc3MTkxMDcxNzc0.YiIEzQ.GV4PVUnAFQMtPWE0BLXggO0Z1ZU', //your discord bot token
         px: 'vp!',
         playing: 'Voxy Music',
 
@@ -9,7 +9,7 @@ module.exports = {
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
-        maxVol: 250, //You can specify the maximum volume level.
+        maxVol: 100, //You can specify the maximum volume level.
         loopMessage: false, //Please don't touch
         discordPlayer: {
             ytdlOptions: {
